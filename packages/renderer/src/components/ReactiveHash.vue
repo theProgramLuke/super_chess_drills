b5677b93eb0a5453b2d6446fc6e352ef9cdd7d71
theProgramLuke <4632542+theProgramLuke@users.noreply.github.com>
2022-02-06 11:39:18 -0600

    apply prettier

--- packages/renderer/src/components/ReactiveHash.vue
+++ packages/renderer/src/components/ReactiveHash.vue
@@ -1,30 +1,25 @@
 <script lang="ts" setup>
-import {computed, ref} from 'vue';
+import { computed, ref } from "vue";
 
-const rawString = ref('Hello World');
+const rawString = ref("Hello World");
 /**
  * window.nodeCrypto was exposed from {@link module:preload}
  */
-const hashedString = computed(() => window.nodeCrypto.sha256sum(rawString.value));
+const hashedString = computed(() =>
+  window.nodeCrypto.sha256sum(rawString.value),
+);
 </script>
 
 <template>
   <label>
     Raw value
-    <input
-      v-model="rawString"
-      type="text"
-    >
+    <input v-model="rawString" type="text" />
   </label>
-  <br>
+  <br />
   <label>
     Hashed by node:crypto
-    <input
-      v-model="hashedString"
-      type="text"
-      readonly
-    >
+    <input v-model="hashedString" type="text" readonly />
   </label>
-  <br><br>
+  <br /><br />
   <code>packages/renderer/src/components/ReactiveHash.vue</code>
 </template>
