3834ab2bb8167b567b83104b186d974920d54152
theProgramLuke <4632542+theProgramLuke@users.noreply.github.com>
2022-02-08 22:08:57 -0600

    WIP

---
+++ packages/renderer/tests/ChessBoard.spec.ts
@@ -0,0 +1,12 @@
+import { describe, expect, test } from "vitest";
+//import { render, screen } from "@testing-library/vue";
+
+//import ChessBoard from "../src/components/ChessBoard.vue";
+
+describe("ChessBoard", () => {
+  test("tests work", () => {
+    //render(ChessBoard);
+
+    expect(true);
+  });
+});
--- packages/renderer/tests/Test.spec.ts
+++
@@ -1,5 +0,0 @@
-import { expect, test } from "vitest";
-
-test("tests work", async () => {
-  expect(true).toBeTruthy();
-});
