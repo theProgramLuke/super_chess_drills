26e43a33e4254d8aec3d65b7dd312795d028e9b8
theProgramLuke <4632542+theProgramLuke@users.noreply.github.com>
2022-02-13 21:48:55 -0600

    WIP v-model:fen

--- packages/renderer/src/components/EditRepertoire.vue
+++ packages/renderer/src/components/EditRepertoire.vue
@@ -1,4 +1,17 @@
-<template lang="pug" src="/@/components/template/EditRepertoire.pug" />
 <script lang="ts" setup>
+import { ref } from "vue";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import ChessBoard from "/@/components/ChessBoard.vue";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const fen = ref("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 </script>
+
+<template lang="pug">
+q-card
+  q-card-section
+    | Edit {{ fen }}
+
+chess-board(v-model:fen="fen")
+</template>
